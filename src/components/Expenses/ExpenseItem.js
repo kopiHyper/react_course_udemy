ee5999f9f7ffa73393bfe96b3d13b598bf1b2372
kopiHyper <oktawiankopica2001@gmail.com>
2023-06-30 21:27:32 +0200

    Wrapped in <li> tag and delete dummy button

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,23 +1,18 @@
-import { useState } from "react";
 import "../Expenses/ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate.js";
 import Card from "../UI/Card.js";
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
-
-  function clickHandler() {
-    setTitle("Update");
-  }
   return (
-    <Card className="expense-item">
-      <ExpenseDate date={props.date} />
-      <div className="expense-item__description">
-        <h2>{title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
-        <button onClick={clickHandler}>Change Title!</button>
-      </div>
-    </Card>
+    <li>
+      <Card className="expense-item">
+        <ExpenseDate date={props.date} />
+        <div className="expense-item__description">
+          <h2>{props.title}</h2>
+          <div className="expense-item__price">${props.amount}</div>
+        </div>
+      </Card>
+    </li>
   );
 };
 
