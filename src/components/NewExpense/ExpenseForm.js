956a06acfc99181323f79dc3a771090d4fc1eb4d
kopiHyper <oktawiankopica2001@gmail.com>
2023-07-01 16:15:13 +0200

    Small bug fix(Adding strings not numbers fix)

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -23,7 +23,7 @@ const ExpenseForm = (props) => {
 
     const expenseData = {
       title: enteredTitle,
-      amount: enteredAmonut,
+      amount: +enteredAmonut,
       date: new Date(enteredDate),
     };
 
